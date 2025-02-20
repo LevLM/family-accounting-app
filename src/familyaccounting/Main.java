0861e44fec8d8cc55b884a51f336fb03bc041384
unexpectedstuff <unexpectedstuff993@gmail.com>
2025-02-20 17:52:11 +0200

    polish

--- src/familyaccounting/Main.java
+++ src/familyaccounting/Main.java
@@ -1,9 +1,6 @@
 package familyaccounting;
 
 import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.DateTimeParseException;
 
 public class Main {
 	public static void main(String[] args) {
